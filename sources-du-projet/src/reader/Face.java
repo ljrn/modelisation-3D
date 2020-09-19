5a9e57388002d24a08b17ac275095ad298faece2
MARMOUCH <MARMOUCH@DESKTOP-LJAG78D.mshome.net>
2020-09-19 14:50:41 +0200

    Faces creation

--- sources-du-projet/src/reader/Face.java
+++ sources-du-projet/src/reader/Face.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class Face {
 	private int nbPoint;
-	private List<Point> points;
+	private List<Integer> points;
+	
+	public Face(int nb, List<Integer> points) {
+		this.nbPoint = nb;
+		this.points = points;
+	}
+
+	public int getNbPoint() {
+		return nbPoint;
+	}
+
+	public List<Integer> getPoints() {
+		return points;
+	}
+	
 	
 }
